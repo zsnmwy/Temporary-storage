d34e870b0df86acbd629909082640c920ed51106
zsnmwy <szlszl35622@gmail.com>
2018-04-06 21:41:07 +0800

    Get steamcommunity ip
    Make use of Cloudflare DNS over HTTPS to auto update steamcommunity in linux.
    https://developers.cloudflare.com/1.1.1.1/dns-over-https/

---
+++ Add_cron_update_hosts_steamcommunity.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+Get_steamcommunity_ip(){
+	curl 'https://cloudflare-dns.com/dns-query?ct=application/dns-json&name=steamcommunity.com&type=A' | jq -r '.Answer[0].data'
+}
+	get_ip=$(cat /etc/hosts | grep steamcommunity.com | cut -d ' ' -f 1)
+	sed -i -e 's#'"$(echo ${get_ip})"'#'"$(echo $(Get_steamcommunity_ip))"'#' /etc/hosts
+	echo "[$(date "+%Y-%m-%d %H:%M:%S %u %Z")] 已经更新steamcommunity-hosts" >> /tmp/steamcommunity-hosts.log
+	hosts=$(cat /etc/hosts | grep steamcommunity.com)
+	echo -e "[$(date "+%Y-%m-%d %H:%M:%S %u %Z")] 当前的steamcommunicty host 是 \n ${hosts} \n\n" >> /tmp/steamcommunity-hosts.log
