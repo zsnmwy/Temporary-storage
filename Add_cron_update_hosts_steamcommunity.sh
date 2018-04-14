d67b4fcb3fab5eeb97b588ff25ac761f7d3468f1
zsnmwy <szlszl35622@gmail.com>
2018-04-14 08:27:16 +0800

    Get ip jq change to cut

--- Add_cron_update_hosts_steamcommunity.sh
+++ Add_cron_update_hosts_steamcommunity.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 Get_steamcommunity_ip(){
-	curl 'https://cloudflare-dns.com/dns-query?ct=application/dns-json&name=steamcommunity.com&type=A' | jq -r '.Answer[0].data'
+	curl 'https://cloudflare-dns.com/dns-query?ct=application/dns-json&name=steamcommunity.com&type=A' | cut -d '"' -f34
 }
 	get_ip=$(cat /etc/hosts | grep steamcommunity.com | cut -d ' ' -f 1)
 	sed -i -e 's#'"$(echo ${get_ip})"'#'"$(echo $(Get_steamcommunity_ip))"'#' /etc/hosts
